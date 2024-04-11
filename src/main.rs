6296f96ebd7c8e0c05c79b3fa2dd7e278716542d
Bamidele Isaac <bamideleisaac11@gmail.com>
2024-04-11 21:13:30 +0100

    matching bracket in progress

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 /* mod prime_factors; 
 mod bob;
 use bob::bob_risky;
-mod highscore;*/
-
+mod highscore;
 mod kg_garden;
-
 use kg_garden::kindergarten_garden::plants;
+*/
+mod matching_bracket;
+use matching_bracket::matching_brackets;
+
 fn main() {
+    let res = matching_brackets::brackets_are_balanced("{[]}");
+    print!("result is {res:?}");
+    /*
     let diagram = "VRCGVVRVCGGCCGVRGCVCGCGV
-VRCCCGCRRGVCGCRVVCVGCGCV";
+    VRCCCGCRRGVCGCRVVCVGCGCV";
     let student = "Alice";
     let expected = vec!["radishes", "clover", "grass", "grass"];
     println!("whattt {:?}",plants(diagram, student));
-   /*  let prime_factors = prime_factors::factors(93819012551);
+     let prime_factors = prime_factors::factors(93819012551);
     println!("{:?}",prime_factors);  
     println!("{:?}",bob_risky::reply("1, 2, 3 GO!"));*/
 }
--- src/matching_bracket.rs
+++ src/matching_bracket.rs
@@ -1,9 +1,40 @@
-#[doc]
+pub mod matching_brackets{
+
+use std::collections::HashMap;
 ///Instructions
 ///Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly. The string may also contain other characters, which for the purposes of this exercise should be ignored.
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut pair_map = HashMap::new();
+    let pairs:[(char,char);3] = [('[',']'),('{','}'),('(',')')];
+    for (lb,rb) in &pairs{
+        pair_map.insert(lb, (lb,rb));
+    }
+    let string_char:Vec<char> = string.chars().collect();
+    let length = string_char.len();
+    if length%2==0{
+        let mut midpoint = length/2;
+        while midpoint>0 {
+            let (lp,rp) = (string_char[midpoint-1],string_char[midpoint]);
+            print!("lp,rp is {}{}",lp,rp);
+            match pair_map.get(&lp){
+                    Some(val)=>{
+                        if *val.0==lp && *val.1==rp{
+                            midpoint=midpoint-1;
+                        }else{
+                            return  false;
+                        }
+                    },
+                    None=> {
+                       return false;
+                    }
+                };
+        };
+        return true;
+    }
+    false
+}
+
 }
 
 
@@ -13,92 +44,74 @@ fn paired_square_brackets() {
     assert!(brackets_are_balanced("[]"));
 }
 #[test]
-#[ignore]
 fn empty_string() {
     assert!(brackets_are_balanced(""));
 }
 #[test]
-#[ignore]
 fn unpaired_brackets() {
     assert!(!brackets_are_balanced("[["));
 }
 #[test]
-#[ignore]
 fn wrong_ordered_brackets() {
     assert!(!brackets_are_balanced("}{"));
 }
 #[test]
-#[ignore]
 fn wrong_closing_bracket() {
     assert!(!brackets_are_balanced("{]"));
 }
 #[test]
-#[ignore]
 fn paired_with_whitespace() {
     assert!(brackets_are_balanced("{ }"));
 }
 #[test]
-#[ignore]
 fn partially_paired_brackets() {
     assert!(!brackets_are_balanced("{[])"));
 }
 #[test]
-#[ignore]
 fn simple_nested_brackets() {
     assert!(brackets_are_balanced("{[]}"));
 }
 #[test]
-#[ignore]
 fn several_paired_brackets() {
     assert!(brackets_are_balanced("{}[]"));
 }
 #[test]
-#[ignore]
 fn paired_and_nested_brackets() {
     assert!(brackets_are_balanced("([{}({}[])])"));
 }
 #[test]
-#[ignore]
 fn unopened_closing_brackets() {
     assert!(!brackets_are_balanced("{[)][]}"));
 }
 #[test]
-#[ignore]
 fn unpaired_and_nested_brackets() {
     assert!(!brackets_are_balanced("([{])"));
 }
 #[test]
-#[ignore]
 fn paired_and_wrong_nested_brackets() {
     assert!(!brackets_are_balanced("[({]})"));
 }
 #[test]
-#[ignore]
 fn paired_and_incomplete_brackets() {
     assert!(!brackets_are_balanced("{}["));
 }
 #[test]
-#[ignore]
 fn too_many_closing_brackets() {
     assert!(!brackets_are_balanced("[]]"));
 }
 #[test]
-#[ignore]
 fn early_incomplete_brackets() {
     assert!(!brackets_are_balanced(")()"));
 }
 #[test]
-#[ignore]
 fn early_mismatched_brackets() {
     assert!(!brackets_are_balanced("{)()"));
 }
 #[test]
-#[ignore]
 fn math_expression() {
     assert!(brackets_are_balanced("(((185 + 223.85) * 15) - 543)/2"));
 }
 #[test]
-#[ignore]
 fn complex_latex_expression() {
     let input = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \
                  \\end{array}\\right)";
